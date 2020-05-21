9e5e8e21f47cec11758f40b0dcfdaa0b9bbb9bb7
Sultan Faisal <faisalbashir.ebox@gmail.com>
2020-05-21 07:30:26 +0100

    setup completed

---
+++ eshopbackend/src/main/java/com/kzn/eshopbackend/App.java
@@ -0,0 +1,13 @@
+package com.kzn.eshopbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
